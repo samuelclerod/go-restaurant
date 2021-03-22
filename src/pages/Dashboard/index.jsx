b246af21f1c6c0632c6fdd50d91cb30775254bff
Samuel Rodrigues <samuelclerod@gmail.com>
2021-03-22 08:47:11 -0300

    reafactor: Header component

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,8 +1,8 @@
 import { Component } from 'react';
 
-import Header from '../../components/Header';
-import api from '../../services/api';
+import { Header } from '../../components/Header';
 import { Food } from '../../components/Food';
+import api from '../../services/api';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
